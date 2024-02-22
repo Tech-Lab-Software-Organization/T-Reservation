8c92a2f58ef16c1f3d6a9b1be49c25857db6430b
Yulissa22 <ec1413220@gmail.com>
2024-02-21 21:57:30 -0600

    Modelo Restaurante

---
+++ Models/Restaurante.cs
@@ -0,0 +1,20 @@
+﻿namespace T_Reservation.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Restaurante
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El Descripcion es obligatorio")]
+        [StringLength(500)]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El Direccion es obligatorio")]
+        [StringLength(250)]
+        public string Direccion { get; set; }
+    }
+
