f0ac41747c7bc925ea21468ac87f71b68dcf95a3
Jose47s <rh24003@esfe.agape.edu.sv>
2024-02-21 22:16:39 -0600

    Modelo Empleado

---
+++ Models/Empleado.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace T_Reservation.Models
+{
+    public class Empleado
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(50, ErrorMessage = "El nombre no puede superar los 50 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El DUI es obligatorio")]
+        [RegularExpression("[0-9]{9}", ErrorMessage = "El DUI debe tener 9 dígitos")]
+        public int Dui { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria")]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "La dirección es obligatoria")]
+        [StringLength(100, ErrorMessage = "La dirección no puede superar los 100 caracteres")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio")]
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido")]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+        [RegularExpression("[0-9]{8}", ErrorMessage = "El teléfono debe tener 8 dígitos")]
+        public int Telefono { get; set; }
+
+        [Required(ErrorMessage = "El rol es obligatorio")]
+        public string Rol { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "La contraseña debe tener entre 8 y 20 caracteres")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public int RestauranteId { get; set; }
+
+        //public Restaurante Restaurante { get; set; }
+    }
+}
