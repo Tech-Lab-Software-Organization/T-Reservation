06d7aa176d309512df427266eff69090ebdf064b
mm24005 <mm24005@esfe.agape.edu.sv>
2024-02-21 21:51:34 -0600

    Modelo Mesa

---
+++ Models/Mesa.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace T_Reservation.Models
+{
+    public class Mesa
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required (ErrorMessage = "El campo {0} es requerido")]
+        //[ForeignKey ("RestauranteId")]
+        public int RestauranteId { get; set; }
+        //[ForeignKey ("RestauranteId")]
+        //public Restaurantes restaurantes { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+
+        public int Numero { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+
+        public int Capacidad { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+
+        [StringLength(100)]
+        public string Area { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        
+        public string Disponibilidad { get; set; }
+        
+
+    }
+}
