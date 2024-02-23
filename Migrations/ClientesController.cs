0d950c172dd0ec2b176dd2e0badbf6ea7b8d8104
yeraldycarpio <yeraldycarpio_morales@hotmail.com>
2024-02-23 08:28:52 -0600

    controlador y vista Clientes

---
+++ Migrations/ClientesController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using T_Reservation.Models;
+
+namespace T_Reservation.Migrations
+{
+    public class ClientesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Clientes
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Clientes.Include(c => c.Restaurante);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Clientes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Clientes == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes
+                .Include(c => c.Restaurante)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // GET: Clientes/Create
+        public IActionResult Create()
+        {
+            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Descripcion");
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Dui,Telefono,Correo,Direccion,FechaNacimiento,Passaword,RestauranteId")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Descripcion", cliente.RestauranteId);
+            return View(cliente);
+        }
+
+        // GET: Clientes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Clientes == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Descripcion", cliente.RestauranteId);
+            return View(cliente);
+        }
+
+        // POST: Clientes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Dui,Telefono,Correo,Direccion,FechaNacimiento,Passaword,RestauranteId")] Cliente cliente)
+        {
+            if (id != cliente.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cliente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteExists(cliente.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Descripcion", cliente.RestauranteId);
+            return View(cliente);
+        }
+
+        // GET: Clientes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Clientes == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes
+                .Include(c => c.Restaurante)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Clientes == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Clientes'  is null.");
+            }
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente != null)
+            {
+                _context.Clientes.Remove(cliente);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClienteExists(int id)
+        {
+          return _context.Clientes.Any(e => e.Id == id);
+        }
+    }
+}
