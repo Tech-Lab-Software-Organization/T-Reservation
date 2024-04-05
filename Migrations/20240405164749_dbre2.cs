8714e371c42f9a0c3e5dfe2a2eedaf7004f77129
Ingrid Jacqueline Gabriel Zepeda <ingridgabriel30@hotmail.com>
2024-04-05 11:01:14 -0600

    Base de datos remoto.

---
+++ Migrations/20240405164749_dbre2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace T_Reservation.Migrations
+{
+    public partial class dbre2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
