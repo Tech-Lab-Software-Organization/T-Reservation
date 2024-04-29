87a9d3f610fd9932ffceedcd86b8e8f3aa215207
KevinSilva777 <sb24002@esfe.agape.edu.sv>
2024-04-29 11:49:50 -0600

    DAL de Menu

---
+++ T-RESERVATION.AccesoDatos/T-RESERVATIONMenuDAL.cs
@@ -0,0 +1,60 @@
+using T_RESERVATION.EntidadesNegocio;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace T_RESERVATION.AccesoDatos;
+
+public class MenuDAL{
+    private readonly ApplicationDbContext _context;
+
+    public MenuDAL(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Menu>> GetAllMenusAsync()
+        {
+            return await _context.Menu.Include(m => m.Restaurante).ToListAsync();
+        }
+        public async Task<Menu> GetMenuByIdAsync(int id)
+        {
+            return await _context.Menu.Include(m => m.Restaurante).FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task CreateMenuAsync(Menu menu)
+        {
+            
+            _context.Add(menu);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateMenuAsync(Menu menu)
+        {
+            var existingMenu = await _context.Menu.FirstOrDefaultAsync(s => s.Id == menu.Id);
+
+            if (existingMenu == null)
+            {
+                throw new InvalidOperationException("Menu not found");
+            }
+
+            
+
+            existingMenu.Producto = menu.Producto;
+            existingMenu.Descripcion = menu.Descripcion;
+            existingMenu.NotaEspecial = menu.NotaEspecial;
+            existingMenu.Precio = menu.Precio;
+            existingMenu.RestauranteId = menu.RestauranteId;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteMenuAsync(int id)
+        {
+            var menu = await _context.Menu.FindAsync(id);
+            if (menu == null)
+            {
+                throw new InvalidOperationException("Menu not found");
+            }
+            _context.Menu.Remove(menu);
+            await _context.SaveChangesAsync();
+        }
+}
